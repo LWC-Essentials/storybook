fa6ce392fc2bc0468d6eabd031c6e5a080a7c421
Philippe Riand <priand@salesforce.com>
2020-07-13 22:12:41 -0400

    Fixed the build for the project and packages

---
+++ packages/lwc-library/src/main.js
@@ -0,0 +1,6 @@
+import "hello/world"
+import "hello/greetings"
+import "hello/time"
+
+// Msin entry point fo the library, although not effectively used
+// This is more for checking that there is not error in the code...
